390cf93fdac323b7c93cd952fd426883193d375b
Doublepor <doublepor.pp@gmail.com>
2021-01-17 23:45:08 +0700

    deploy GCP

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as Express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+const server = Express();
+server.get('/', (req, res) => res.send('ok'));
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  const config = new DocumentBuilder()
+    .setTitle('REST API example')
+    .setDescription('The REST API description')
+    .setVersion('1.0')
+    .addTag('Member Manalab')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
 }
 bootstrap();
